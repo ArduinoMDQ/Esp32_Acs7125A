4bac585ac0bca4fae9f01fea6297d6e76c3cd7af
diegotestereo <diegotestereo@gmail.com>
2017-10-23 10:59:25 -0300

    fix

--- Esp32_Acs7125A.ino
+++ Esp32_Acs7125A.ino
@@ -1,15 +1,16 @@
 
 const int analogPin = 35;  // Analog input pin 
-double mVperAmp = 0.185;//185.0; // use 100 for 20A****66 for 30A **** 185 for 5A
+float mVperAmp = 0.185;//185.0; // use 100 for 20A****66 for 30A **** 185 for 5A
 double Voltage = 0;
-double VRMS = 0;
-double AmpsRMS = 0;
+float VRMS = 0;
+float AmpsRMS = 0;
 int muestrasPromedio=0;
 const int nUmb = 4095;
-const double vcc =3.3;
-const double vEsc = vcc/nUmb ;
-const float minUmbral =0.020;
-const float correccion =0;//0.025;
+const float vcc =3.3;
+const float vEsc = vcc/nUmb ;
+const float minUmbral =0.03;
+const float correccion =0.000;
+const int seg = 3;
 
 ////******** FIN ACS712////////
 
@@ -17,17 +18,17 @@ void task_ADC( void * parameter ){
   
   while(1){
     Serial.println("****************************************");
-    Serial.print("Valor Escalon 3.3/");Serial.print(nUmb,1);Serial.print(":");Serial.println(vEsc,8);
+    Serial.print("V Escalon :");Serial.println(vEsc,8);
+    Serial.print("12 bits -> ");Serial.println(nUmb);
     float valor = TrueRMSMuestras();
     AmpsRMS=valor/mVperAmp - correccion;
     Serial.print("AmpsRMS medido: "); Serial.println(AmpsRMS,3);
     if(AmpsRMS < minUmbral){  AmpsRMS=0;}  
-    String Potencia = String(220*AmpsRMS);
-    String Corriente = String(AmpsRMS);
+    float Potencia = 220*AmpsRMS;
     Serial.print("AmpsRMS RMS: "); Serial.println(AmpsRMS,3);
-    Serial.println("POWER RMS: " + String(Potencia));
+    Serial.print("POWER RMS: ");Serial.println(Potencia,2);
    
-    delay(3000);
+    delay(seg*1000);
   }
 }
 
@@ -41,20 +42,22 @@ void loop() {
 
 float TrueRMSMuestras(){
   
- double result =0,conv=0,Acumulador=0,suma=0;
+ float result =0,conv=0,Acumulador=0,suma=0;
  int readValue =0;         
  int Count = 0;
  int promedio = 0;
- double promedioRead = 0;
+ float promedioRead = 0;
  int sumatoria = 0;
- const int n = 320;
- int diferencia =0;
- int signo = 0;
- double Vo =0;
+ const int n = 2000;
+ const int m = 2000;
  
+ 
+ int signo = 0;
+ float Vo =0;
+ int diferencia = 0;
  
  for(int i=0;i<n;i++){
-    delay(1);
+    delay(.1);
     sumatoria = sumatoria + analogRead(analogPin);
   } 
  
@@ -65,22 +68,21 @@ float TrueRMSMuestras(){
  Vo=promedio*vEsc;
  Serial.print("Vo : "); Serial.println(Vo,3);
  uint32_t start_time = millis();
- while(Count < 320){     
-    delay(1);
+ while(Count < m){     
+     delay(.1);
      Count++;
      readValue =  analogRead(analogPin);
-     promedioRead=promedioRead + readValue;
-     signo = promedio-2793;
-     diferencia=abs(signo);
-     
-    
-     conv=(readValue-signo)*vEsc - 2.5;//2793= 2.25v 
-    
+     diferencia = abs(readValue - promedio);
+     if(diferencia<10){
+       diferencia = 0;
+     }
+     conv=diferencia*vEsc;// 2.25;//2793= 2.25v 
      Acumulador=Acumulador+sq(conv);  
      }
    suma=Acumulador/Count;
+   Serial.print("Acumulador: ");Serial.println(Acumulador,3);
+   Serial.print("Count: ");Serial.println(Count);
    result=sqrt(suma);
-   Serial.println("Count: "+String(Count)+"Muestras");
    Serial.print("analogRead(analogPin): "); Serial.println(analogRead(analogPin));
    Serial.println(String(n)+" Muestras Promedio : "+String(promedio));
   return result;
